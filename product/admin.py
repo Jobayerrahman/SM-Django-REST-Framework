bc401c669bdfbca971a1217a567be024e6224d6b
jobayerrahman <jobayer.aiub@gmail.com>
2024-01-04 18:54:51 +0600

    Django REST Framework initial commit

---
+++ product/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . models import Product
+
+# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['id','product_name','product_price','product_amount','group_name']
